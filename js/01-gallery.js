a5bacf02bed3763b6e467d0bf115e07471858ed4
Maks Thimbarov <imaksim2@icloud.com>
2022-11-27 01:55:51 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryDiv = document.querySelector(".gallery");
+galleryDiv.innerHTML += onCreatePlot(galleryItems);
+function onCreatePlot(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${element.original}" rel="noopner">
+            <img
+                class="gallery__image"
+                src="${element.preview}"
+                data-source="${element.original}"
+                alt="${element.description}"
+                
+            />
+        </a>
+    </div>`;
+    })
+    .join("");
+}
+galleryDiv.addEventListener("click", onItemClick);
+function onItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
-console.log(galleryItems);
+  const modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="1400" height="900">`
+  );
+  modal.show();
+  galleryDiv.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      modal.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML += onCreatePlot(galleryItems);
+function onCreatePlot(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `<a class="gallery__item" href="${element.original}">
+        <img class="gallery__image" src="${element.preview}" alt="${element.description}"/>
+        </a>`;
+    })
+    .join("");
+}
 
-console.log(galleryItems);
+let modal = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+modal.on("show.simplelightbox");
